912e991b4fef1dcad537b32c08a0cd0fe01ee395
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-10-31 17:28:17 +0300

    feat(#1174): ChPattern - naive implementation

--- eo-maven-plugin/src/main/java/org/eolang/maven/ChPattern.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/ChPattern.java
@@ -1,5 +1,8 @@
 package org.eolang.maven;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ChPattern implements CommitHash {
 
     private final String pattern;
@@ -15,6 +18,53 @@ public class ChPattern implements CommitHash {
 
     @Override
     public String value() {
-        return null;
+        final String[] allPatterns = pattern.split(",");
+        for (final String pattern : allPatterns) {
+            final String[] keyValue = pattern.split(":");
+            String key = keyValue[0];
+            String value = keyValue[1];
+            if (new PatternEntry(key, value).matches(tag)) {
+                return value;
+            }
+        }
+        return "";
+    }
+
+    private static class PatternEntry {
+
+        private final String tag;
+        private final String hash;
+
+        private PatternEntry(final String tag, final String hash) {
+            this.tag = tag;
+            this.hash = hash;
+        }
+
+        private boolean matches(final String incoming) {
+            if (incoming.equals(tag)) {
+                return true;
+            }
+            final String[] splitPattern = tag.split("\\.");
+
+            List<String> keys = new LinkedList<>();
+            for (final String s : splitPattern) {
+                if (s.equals("*")) {
+                    keys.add("\\w+");
+                } else {
+                    keys.add(s);
+                }
+            }
+
+            final String compiled = String.join("\\.", keys);
+
+            System.out.println(splitPattern);
+            System.out.println(compiled);
+            final boolean res = incoming.matches(compiled);
+            return res;
+        }
+
+        private String hash() {
+            return hash;
+        }
     }
 }
--- eo-maven-plugin/src/test/java/org/eolang/maven/ChPatternTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/ChPatternTest.java
@@ -10,6 +10,7 @@ class ChPatternTest {
     @ParameterizedTest
     @CsvSource({
         "'0.*.*:abc2sd3', '0.0.0', abc2sd3",
+        "'*.*.*:abc2sd3', '2.2.2', abc2sd3",
         "'0.*.*:abc2sd3', '1.0.0', ''",
         "'0.*.*:m23ss3h', '0.1.2', 'm23ss3h'",
         "'0.*.*:m23ss3h,1.*.*:abc2sd3', '0.1.2', 'm23ss3h'",
