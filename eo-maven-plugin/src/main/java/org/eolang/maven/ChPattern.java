16d39b225527f90692a80d30dc01cc66a603df86
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-10-31 17:43:16 +0300

    feat(#1174): add correct implementation of ChPattern. All tests pass
    feat(#1174): remove redundant classes
    
    feat(#1174): simplify ChPattern
    
    feat(#1174): simplify ChPattern
    
    feat(#1174): simplify ChPattern

--- eo-maven-plugin/src/main/java/org/eolang/maven/ChPattern.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/ChPattern.java
@@ -1,7 +1,13 @@
 package org.eolang.maven;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+import org.cactoos.iterable.IterableOf;
+import org.cactoos.iterable.Mapped;
 
 public class ChPattern implements CommitHash {
 
@@ -18,53 +24,69 @@ public class ChPattern implements CommitHash {
 
     @Override
     public String value() {
-        final String[] allPatterns = pattern.split(",");
-        for (final String pattern : allPatterns) {
-            final String[] keyValue = pattern.split(":");
-            String key = keyValue[0];
-            String value = keyValue[1];
-            if (new PatternEntry(key, value).matches(tag)) {
-                return value;
+        final SortedMap<Integer, String> matches = new TreeMap<>(Comparator.reverseOrder());
+        Iterable<Pattern> all = new Mapped<>(
+            Pattern::new,
+            new IterableOf<>(pattern.split(","))
+        );
+        for (final Pattern pattern : all) {
+            final int weight = pattern.weight(tag);
+            if (weight > 0) {
+                matches.put(weight, pattern.hash);
             }
         }
-        return "";
+        final String hash;
+        if (matches.isEmpty()) {
+            hash = "";
+        } else {
+            hash = matches.get(matches.firstKey());
+        }
+        return hash;
     }
 
-    private static class PatternEntry {
+    private static class Pattern {
 
-        private final String tag;
+        private final String pattern;
         private final String hash;
 
-        private PatternEntry(final String tag, final String hash) {
-            this.tag = tag;
+        private Pattern(String raw) {
+            this(raw.split(":"));
+        }
+
+        private Pattern(String[] raw) {
+            this(raw[0], raw[1]);
+        }
+
+        private Pattern(final String pattern, final String hash) {
+            this.pattern = pattern;
             this.hash = hash;
         }
 
-        private boolean matches(final String incoming) {
-            if (incoming.equals(tag)) {
-                return true;
+        private int weight(final String tag) {
+            final int weight;
+            if (tag.matches(this.regex())) {
+                weight = 1 + numberOfConstants();
+            } else {
+                weight = 0;
             }
-            final String[] splitPattern = tag.split("\\.");
-
-            List<String> keys = new LinkedList<>();
-            for (final String s : splitPattern) {
-                if (s.equals("*")) {
-                    keys.add("\\w+");
-                } else {
-                    keys.add(s);
-                }
-            }
-
-            final String compiled = String.join("\\.", keys);
-
-            System.out.println(splitPattern);
-            System.out.println(compiled);
-            final boolean res = incoming.matches(compiled);
-            return res;
+            return weight;
         }
 
-        private String hash() {
-            return hash;
+        private int numberOfConstants() {
+            return (int) Stream.of(pattern.split("\\."))
+                .filter(s -> !s.equals("*")).count();
+        }
+
+        private String regex() {
+            List<String> keys = new LinkedList<>();
+            for (final String key : pattern.split("\\.")) {
+                if (key.equals("*")) {
+                    keys.add("\\w+");
+                } else {
+                    keys.add(key);
+                }
+            }
+            return String.join("\\.", keys);
         }
     }
 }
